1998d21073acda8cc74c03aaef03b01449a14d4d
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2026-04-01 15:48:32 +0530

    implemented grpc gateway trip client

---
+++ services/api-gateway/grpc_clients/trip_client.go
@@ -0,0 +1,37 @@
+package grpc_clients
+
+import (
+	"os"
+
+	"google.golang.org/grpc"
+	pb "ride-sharing/shared/proto/trip"
+)
+
+type tripServiceClient struct {
+	Client pb.TripServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewTripServiceClient() (*tripServiceClient, error) {
+	tripServiceURL := os.Getenv("TRIP_SERVICE_URL")
+
+	conn, err := grpc.NewClient(tripServiceURL)
+	if err != nil {
+		return nil, err
+	}
+
+	client := pb.NewTripServiceClient(conn)
+
+	return &tripServiceClient{
+		Client: client,
+		conn:   conn,
+	}, nil
+}
+
+func (c *tripServiceClient) Close() {
+	if c.conn != nil {
+		if err := c.conn.Close(); err != nil {
+			return
+		}
+	}
+}
